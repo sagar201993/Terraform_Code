8152bf2c61975767a7527c95effd7cc4e27e8487
sagar <124078739+sagar201993@users.noreply.github.com>
2026-06-30 15:09:59 +0200

    created public subnet

--- createvpc/main.tf
+++ createvpc/main.tf
@@ -7,3 +7,15 @@ resource "aws_vpc" "dev_vpc" {
     ManagedBy   = "Terraform"
   }
 }
+# create public subnet 
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name        = "dev-public-subnet"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
